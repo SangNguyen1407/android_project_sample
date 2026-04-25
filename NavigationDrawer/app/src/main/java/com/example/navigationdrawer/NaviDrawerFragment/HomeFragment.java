6de9de11933eb958b0a7f2dd6850437e775be44d
Sang <14.sangnguyen.07@gmail.com>
2026-04-25 14:59:45 +0900

    add listview

--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/NaviDrawerFragment/HomeFragment.java
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/NaviDrawerFragment/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +13,40 @@ import androidx.fragment.app.Fragment;
 
 import com.example.navigationdrawer.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ListView lvView = view.findViewById(R.id.lvMenu);
+        List<Map<String,String>> menuList = new ArrayList<>();
+
+        // Create and add the first menu item
+        Map<String, String> item1 = new HashMap<>();
+        item1.put("name", "ラーメン");
+        item1.put("price", "2.000");
+        menuList.add(item1);
+        menuList.add(item1);
+        menuList.add(item1);
+
+        String[] from = new String[] {"name", "price"};
+        int[] to = new int[] {R.id.row_title, R.id.row_subTitle};
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                getActivity(),
+                menuList,
+                R.layout.list_menu,
+                from,
+                to
+        );
+        lvView.setAdapter(adapter);
+
+        return view;
     }
 }
