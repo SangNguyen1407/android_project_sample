9c88220ef9289f3569174ed9def45ac831e87af7
SangNguyen1407 <14.sangnguyen.07@gmail.com>
2026-04-26 17:23:58 +0900

    update list menu

--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/NaviDrawerFragment/HomeFragment.java
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/NaviDrawerFragment/HomeFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,13 +31,21 @@ public class HomeFragment extends Fragment {
 
         // Create and add the first menu item
         Map<String, String> item1 = new HashMap<>();
-        item1.put("name", "ラーメン");
-        item1.put("price", "2.000");
-        menuList.add(item1);
-        menuList.add(item1);
+        item1.put("name", "東京");
+        item1.put("id", "270000");
         menuList.add(item1);
 
-        String[] from = new String[] {"name", "price"};
+        Map<String, String> item2 = new HashMap<>();
+        item2.put("name", "大阪");
+        item2.put("id", "280000");
+        menuList.add(item2);
+
+        Map<String, String> item3 = new HashMap<>();
+        item3.put("name", "神戸");
+        item3.put("id", "290000");
+        menuList.add(item3);
+
+        String[] from = new String[] {"name", "id"};
         int[] to = new int[] {R.id.row_title, R.id.row_subTitle};
 
         SimpleAdapter adapter = new SimpleAdapter(
@@ -46,7 +56,20 @@ public class HomeFragment extends Fragment {
                 to
         );
         lvView.setAdapter(adapter);
+        lvView.setOnItemClickListener(new ListItemClickListener());
 
         return view;
+    }
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//            ListViewでタップされた行の都市名と都市IDを取得
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            String cityName = item.get("name");
+            String cityId = item.get("id");
+
+            TextView tvCityName = view.findViewById(R.id.tvCityName);
+            tvCityName.setText("       天気：");
+        }
     }
 }
